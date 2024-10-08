6c795a760849f7ed232a406d9194cfb6810b963e
egene-chung <egenechung@gmail.com>
2024-10-08 20:02:23 +0900

    Modify: 프리티어 적용

--- src/pages/SignUp.js
+++ src/pages/SignUp.js
@@ -1,18 +1,19 @@
 // SignUp.js
-import React from 'react';
-import { useNavigate } from 'react-router-dom'; // useNavigate 훅 import
+import React from "react";
+import { useNavigate } from "react-router-dom"; // useNavigate 훅 import
 import loginImage from "../image/kakao_login_medium_narrow.png";
 
 function SignUp() {
   const navigate = useNavigate(); // useNavigate 훅 사용
   const handleButtonClick = () => {
     // 카카오 로그인 버튼 클릭 시 외부 사이트로 이동
-    window.location.href = 'https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code'; 
+    window.location.href =
+      "https://kauth.kakao.com/oauth/authorize?client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code";
   };
 
   // 메인 페이지 이동 버튼 클릭 시 메인 페이지로 리디렉션
   const handleMainPageClick = () => {
-    navigate('/main'); // /main 경로로 이동
+    navigate("/main"); // /main 경로로 이동
   };
 
   return (
@@ -31,26 +32,26 @@ function SignUp() {
 
 const styles = {
   container: {
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    justifyContent: 'center',
-    height: '100vh',
-    backgroundColor: '#f9f9f9',
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    height: "100vh",
+    backgroundColor: "#f9f9f9",
   },
   mainButton: {
-    marginTop: '20px',
-    padding: '10px 20px',
+    marginTop: "20px",
+    padding: "10px 20px",
     borderRadius: 5,
-    border: 'none',
-    backgroundColor: '#5469c1',
-    color: '#fff',
-    cursor: 'pointer',
+    border: "none",
+    backgroundColor: "#5469c1",
+    color: "#fff",
+    cursor: "pointer",
   },
   image: {
-    display: 'block',
-    width: '200px',
-    height: 'auto',
+    display: "block",
+    width: "200px",
+    height: "auto",
   },
 };
 

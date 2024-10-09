d105c09d090b8195ec8b13ae37ac27d4494bb400
egene-chung <egenechung@gmail.com>
2024-10-09 22:05:09 +0900

    Modify: 카카오 로그인하기 구현

--- src/components/OAuth2RedirectHandler.js
+++ src/components/OAuth2RedirectHandler.js
@@ -1,19 +1,24 @@
 import React, { useEffect } from "react";
+import { useNavigate, useLocation } from "react-router-dom";
 import axios from "axios";
 import Spinner from "./Spinner";
 
-const code = new URL(window.location.href).searchParams.get("code");
-
 function OAuth2RedirectHandler(props) {
+  const navigate = useNavigate();
   useEffect(() => {
-    axios
-      .get(`https://api.carefli.p-e.kr/kakao?code=${code}`)
-      .then((response) => {
-        console.log("API Response:", response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
+    const getToken = async () => {
+      const code = new URL(window.location.href).searchParams.get("code");
+      axios
+        .get(`https://api.carefli.p-e.kr/api/oauth2/kakao?code=${code}`)
+        .then((response) => {
+          console.log("API Response:", response.data);
+          navigate("/main");
+        })
+        .catch((error) => {
+          console.error("Error fetching data:", error);
+        });
+    };
+    getToken();
   }, []);
 
   return <Spinner />;

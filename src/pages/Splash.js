8f6186a3c6bea1b38eae2bc665c7372e63e74da0
yunjeee <100953788+yunjeee@users.noreply.github.com>
2024-10-12 17:51:12 +0900

    Modify: Splash Screen 로고 이미지로 변경

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import './SplashScreen.css'; // 스타일을 위한 별도의 CSS 파일
-import haemstar from '../image/haemstar.jpg'
+import logo from "../image/carefLi_logo.svg";
 
 const SplashScreen = () => {
     return (
         <div className="splash-screen">
-            <h1>스플래시 페이지</h1> 
-            <img src={haemstar} alt="" />
+            <img src={logo} alt="carefLi logo" />
+            <h1 className='logo-text'>CarefLi</h1>
 
         </div>
     );

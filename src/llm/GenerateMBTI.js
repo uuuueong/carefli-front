e6f703333bb42adb8891dd634c7da966eb63620e
egene-chung <egenechung@gmail.com>
2024-10-15 17:13:23 +0900

    Add: mbti distribution 추가

--- src/llm/GenerateMBTI.js
+++ src/llm/GenerateMBTI.js
@@ -23,6 +23,36 @@ function GenerateMBTI({ name, age, relationship, setMBTI }) {
     { role: "user", content: "Guess the MBTI." },
   ];
 
+  const mbtiDistribution = {
+    ISFJ: 9.08,
+    ISTJ: 8.89,
+    INFP: 8.07,
+    INFJ: 7.68,
+    ENFP: 7.36,
+    ISFP: 7.13,
+    ENFJ: 6.61,
+    ESFJ: 6.31,
+    ESTJ: 6.11,
+    ISTP: 5.97,
+    INTJ: 5.37,
+    ESFP: 5.21,
+    INTP: 4.92,
+    ENTJ: 4.87,
+    ENTP: 3.61,
+    ESTP: 2.81,
+  };
+
+  // const messages = [
+  //   {
+  //     role: "system",
+  //     content: `너는 16가지 MBTI 성격 유형 중 하나를 추측하는 챗봇이야.
+  //     ${name}님은 ${age}살이고, 나와 ${relationship} 관계입니다.
+  //     MBTI 분포 데이터: ${JSON.stringify(mbtiDistribution)}를 참고하여 어떤 유형의 MBTI 일지 추측해줘.
+  //     4글자의 MBTI 유형으로 추측하고, ${mbtiList}에 있는 MBTI 말고 다른 유형으로 추측해줘.`,
+  //   },
+  //   { role: "user", content: "MBTI를 추측해줘." },
+  // ];
+
   const gptInput = {
     model: "gpt-4o-mini",
     temperature: 0.8,

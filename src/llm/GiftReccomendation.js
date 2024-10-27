988bc3062f882f8bffa763aac0a7e02da99145c1
egene-chung <egenechung@gmail.com>
2024-10-27 22:07:34 +0900

    Modify: deprecated code 정리

--- src/llm/GiftReccomendation.js
+++ src/llm/GiftReccomendation.js
@@ -2,52 +2,6 @@ import React, { useEffect, useMemo, useState } from "react";
 import { getMbtiGiftPreference } from "./AssistPrompt";
 import presentWho from "../image/presentWho.gif";
 
-// 선물 리스트 받아오기
-const present_list = [
-  {
-    id: 1,
-    name: "달바 '비건' 오리지널 미스트 세럼 선물 세트 (100ml+50ml)",
-    price: 31900,
-    occasionType: "생일",
-    category: "뷰티",
-    url: "https://gift.kakao.com/product/5392498?tab=review&sortProperty=SCORE",
-  },
-  {
-    id: 2,
-    name: "나만의 비어캔 맥주잔 특별한 각인 술잔 남자 여자 친구 생일 자취 직장인 이사 신혼부부 선물 커플 하이볼잔 남친",
-    price: 23900,
-    occasionType: "생일",
-    category: "주류",
-    url: "https://gift.kakao.com/product/10028370",
-  },
-  {
-    id: 3,
-    name: "아이스 카페 아메리카노 T 2잔 + 부드러운 생크림 카스텔라",
-    price: 13500,
-    occasionType: "생일",
-    category: "커피음료",
-    url: "https://gift.kakao.com/product/7892633",
-  },
-  {
-    id: 4,
-    name: "[홍콩직수입] 제니베이커리 4믹스 쿠키(S) 320g",
-    price: 29500,
-    occasionType: "집들이",
-    category: "디저트",
-    url: "https://gift.kakao.com/product/1263462",
-  },
-  {
-    id: 5,
-    name: "차량용 무선 충전 거치대 핸드폰 휴대폰 자동차 고속 충전기 선물 (오그랩 맥스 차량용 무선충전거치대)",
-    price: 29900,
-    occasionType: "집들이",
-    category: "차량",
-    url: "https://gift.kakao.com/product/2071496",
-  },
-];
-
-// Prop으로 받을 내용
-
 const oneHotEncode = (categories, category) => categories.map((cat) => (cat === category ? 1 : 0));
 
 const cosineSimilarity = (a, b) => {

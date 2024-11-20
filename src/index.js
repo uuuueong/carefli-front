0d68655ac0449d11c1e759fe0b04ff4aa7e10109
yunjeee <100953788+yunjeee@users.noreply.github.com>
2024-11-21 00:28:43 +0900

    Modify: index.js 주석 수정

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode> // 2번씩 찍히는 것: 개발 중에 사이드 이펙트가 제대로 관리되고 있는지 확인하기 위해 useEffect를 포함한 생명주기 메서드를 두 번 실행
 );
 
 // If you want to start measuring performance in your app, pass a function

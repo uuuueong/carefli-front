e8838313f279ff2400d2394745b682f7ed4c7b3d
yunjeee <100953788+yunjeee@users.noreply.github.com>
2024-11-26 21:09:54 +0900

    Modify: indentation 수정

--- src/pages/PersonProfile.js
+++ src/pages/PersonProfile.js
@@ -207,7 +207,7 @@ const styles = {
     alignItems: "center",
     textAlign: "center",
     overflowY: "scroll",
-    height: "77 vh !imprtant",
+    height: "77vh !imprtant",
     // height: "570px",
   },
   profileCard: {
